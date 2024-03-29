9044d0d2539275d7be6206e17ac9089b8f92fd8e
Amit Tiwari <at1484746@gmail.com>
2024-03-29 12:30:51 +0530

    done

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:4000/api/v1/job/getall", {
+        .get("https://seamediabackend-2.onrender.com/api/v1/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
